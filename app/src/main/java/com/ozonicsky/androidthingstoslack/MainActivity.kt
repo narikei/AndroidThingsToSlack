d564c6b40fdb40f09e06c3bb373460838e5ce929
なりたけいすけ <narita_keisuke@cyberagent.co.jp>
2017-07-03 04:06:08 +0900

    Lチカスイッチ

--- app/src/main/java/com/ozonicsky/androidthingstoslack/MainActivity.kt
+++ app/src/main/java/com/ozonicsky/androidthingstoslack/MainActivity.kt
@@ -2,6 +2,10 @@ package com.ozonicsky.androidthingstoslack
 
 import android.app.Activity
 import android.os.Bundle
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.GpioCallback
+import com.google.android.things.pio.PeripheralManagerService
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -25,8 +29,26 @@ import android.os.Bundle
  */
 class MainActivity : Activity() {
 
+    val ledPin: String = "BCM6"
+    val buttonPin: String = "BCM16"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val service = PeripheralManagerService()
+        val led = service.openGpio(ledPin)
+        led.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+        val button = service.openGpio(buttonPin)
+        button.setDirection(Gpio.DIRECTION_IN)
+        button.setActiveType(Gpio.ACTIVE_HIGH)
+        button.setEdgeTriggerType(Gpio.EDGE_BOTH)
+        button.registerGpioCallback(object : GpioCallback() {
+            override fun onGpioEdge(gpio: Gpio): Boolean {
+                led.value = gpio.value != true
+                return true
+            }
+        })
     }
+
 }
